1f8db20775b10a09cffd6af01b7d18d988202b35
Arthur2479 <arthurjacobs1004@gmail.com>
2023-12-02 18:59:34 +0100

    Problem 2 (not finished) with str_to_int

---
+++ advent_of_code_2023/Day_1/Problem_2.py
@@ -0,0 +1,65 @@
+"""
+Your calculation isn't quite right. It looks like some of the digits are actually spelled out with letters: one, two,
+three, four, five, six, seven, eight, and nine also count as valid "digits".
+Equipped with this new information, you now need to find the real first and last digit on each line. For example:
+
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+
+In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Adding these together produces 281.
+
+What is the sum of all of the calibration values?
+"""
+
+import re
+from pathlib import Path
+
+digits_map = {
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9
+}
+
+
+def str_to_int(input_digits: tuple[str, ...]) -> list[int]:
+    output_digits = []
+    for i, digit in enumerate(input_digits):
+        if digit.isdigit():
+            output_digits.append(int(digit))
+            continue
+
+        output_digits.append(digits_map[digit])
+
+    return output_digits
+
+
+def find_digits(line: str):
+    # Regex used : https://regex101.com/r/m3acIR/1. Matches all occurrences of digits (str or int)
+    m: list[str] = re.findall(r'(?=(one|two|three|four|five|six|seven|eight|nine|\d))', line)
+    print(line, m[0], m[-1])
+    return str_to_int((m[0], m[-1]))
+
+
+def sum_of_digits_calibration_values_with_spelled_digits(file_path: Path):
+    with open(file_path) as file:
+        calibration_values = []
+        while line := file.readline()[:-1]:
+            first, last = find_digits(line)
+            calibration_values.append(int(first + last))
+
+        print(f'Sum of calibration values : {sum(calibration_values)}')
+
+
+if __name__ == '__main__':
+    sum_of_digits_calibration_values_with_spelled_digits(file_path=Path('./test_values_2.txt'))
